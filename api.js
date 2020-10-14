4a135842011e8c087b27d5b51b1910dc74ffc4da
usmanjchaudhry <36524989+usmanjchaudhry@users.noreply.github.com>
2020-10-13 19:05:02 -0700

    usman push

--- api.js
+++ api.js
@@ -1,3 +1,3 @@
-$.getJSON(" https://api.covidtracking.com/v1/states/ca/info.json", data => {
-  console.log(data);
+$.getJSON("http://api.covidtracking.com/v1/us/current.json", data => {
+  console.log(data[0]);
 });
