c177a27dda07f9d13e0c4b97e9d06eb7a5f1c035
Michael Moreno <mmoreno2222@gmail.com>
2020-10-15 19:06:05 -0700

    most recent file update

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
     $('#datetimepicker1').datetimepicker();
+
+    $('.reportBtn').onClick(() => {
+        // Need to get user's name or email from cookie
+        //find the user's ID for report
+        const userId = "";
+        const eventName = $('#event-type')
+        const address = $('input#address')
+        const locationType = $('#location-type')
+        const reportDate = $('#datetimepicker1')
+        const note = $('#eventNotes')
+
+        console.log("user: " + userId)
+        console.log("event: " + eventName)
+        console.log("address: " + address)
+        console.log("location: " + locationType)
+        console.log("Report Date: " + reportDate)
+        console.log("Note: " + note)
+
+        // Switch cases
+    })
 });
